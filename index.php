351636384db7456c0baa52fc6b0dea414502ef4b
manojbabu148 <68938401+manojbabu148@users.noreply.github.com>
2021-01-31 13:25:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">       
 </head>
 <body>
+    <h1>ToDoApp</h1>
     <?php require_once 'process.php'?>
 
     <?php 
